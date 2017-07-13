264bca18ab6aa8d4b070541a316c739292c2ea72
rolivieri <roliv@us.ibm.com>
2017-07-13 14:33:47 -0500

    test

--- set-variables.rb
+++ set-variables.rb
@@ -1,14 +1,22 @@
 require 'travis'
+
+organization = "IBM-Swift"
+puts "Initiating program to update environment variables in Travis CI jobs for organization '#{organization}'."
+
+
+puts "#{organization}"
+exit
+
+
 Travis.access_token = Travis.github_auth(ENV['TRAVIS_TOKEN'])
-repos = Travis::Repository.find_all(owner_name: 'oehme')
-	.reject{|repo| repo.slug == 'oehme/travis-secrets-setter'} 
+repos = Travis::Repository.find_all(owner_name: organization)
+	.reject{|repo| repo.slug == 'oehme/travis-secrets-setter'}
 	.select{|repo| Travis.user.admin_access.include?(repo)}
 keys = ['ORG_GRADLE_PROJECT_bintrayApiKey', 'ORG_GRADLE_PROJECT_signingPassword', 'ORG_GRADLE_PROJECT_sonatypePassword']
 repos.each do |repo|
 	keys.each do |key|
 		puts "Setting env var '#{key}' on project '#{repo.slug}'"
-		repo.env_vars.upsert(key, "'#{ENV[key]}'", public: false)
+		#repo.env_vars.upsert(key, "'#{ENV[key]}'", public: false)
 	end
-	repo.env_vars.upsert("GRADLE_OPTS", "'-Xmx1024m -XX:MaxPermSize=512m -Dorg.gradle.daemon=true'", public: true)
-	repo.env_vars.upsert("TERM", "'dumb'", public: true)
+	#repo.env_vars.upsert("TERM", "'dumb'", public: true)
 end
