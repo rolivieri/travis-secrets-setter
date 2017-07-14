8f8707561fc16adf32faaaa935f748c7daa3b09a
rolivieri <roliv@us.ibm.com>
2017-07-14 09:22:32 -0500

    test

--- set-variables.rb
+++ set-variables.rb
@@ -2,9 +2,8 @@ require 'travis'
 
 organization = "IBM-Swift"
 #organization = "rolivieri"
-#
-puts "Initiating program to update environment variables in Travis CI jobs for organization '#{organization}'."
-puts "#{organization}"
+
+puts "About to update public environment variable(s) in Travis CI jobs for organization '#{organization}'."
 
 Travis.access_token = Travis.github_auth(ENV['TRAVIS_TOKEN'])
 repos = Travis::Repository.find_all(owner_name: organization)
@@ -13,10 +12,10 @@ repos = Travis::Repository.find_all(owner_name: organization)
 	#.reject{|repo| repo.slug == 'rolivieri/travis-secrets-setter'}
 	#.reject{|repo| repo.slug == 'rolivieri/get-started-swift'}
 	#.select{|repo| Travis.user.admin_access.include?(repo)}
-keys = ['swift-4-dev-snapshot', 'swift4', 'swift4_dev']
+keys = ['swift_4_dev_snapshot']
 repos.each do |repo|
 	keys.each do |key|
 		puts "Setting env var '#{key}' to '#{ENV[key]}' on project '#{repo.slug}'"
-		repo.env_vars.upsert(key, "'#{ENV[key]}'", public: true)
+		repo.env_vars.upsert(key, "#{ENV[key]}", public: true)
 	end
 end
