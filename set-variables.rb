5543c25d6bb7dc64d19296ce96a7f63e32df9729
rolivieri <roliv@us.ibm.com>
2017-07-14 09:52:27 -0500

    test

--- set-variables.rb
+++ set-variables.rb
@@ -5,7 +5,8 @@ organization = "IBM-Swift"
 
 puts "About to update public environment variable(s) in Travis CI jobs for organization '#{organization}'."
 
-Travis.access_token = Travis.github_auth(ENV['TRAVIS_TOKEN'])
+#Travis.access_token = Travis.github_auth(ENV['TRAVIS_TOKEN'])
+Travis.access_token = Travis.github_auth(ENV['TRAVIS_TOKEN_SWIFTDO'])
 repos = Travis::Repository.find_all(owner_name: organization)
   .select{|repo| repo.slug == 'IBM-Swift/CloudConfiguration'}
 	#.select{|repo| repo.slug == 'rolivieri/db-test'}
