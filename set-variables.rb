ae87cfcbc82234623816e9e41a79f41180b66319
rolivieri <roliv@us.ibm.com>
2017-07-14 08:58:59 -0500

    test

--- set-variables.rb
+++ set-variables.rb
@@ -16,7 +16,7 @@ repos = Travis::Repository.find_all(owner_name: organization)
 keys = ['swift-4-dev-snapshot', 'swift4', 'swift4-dev']
 repos.each do |repo|
 	keys.each do |key|
-		puts "Setting env var '#{key}' to '#{ENV[key]}' on project '#{repo.slug}'"
-		repo.env_vars.upsert(key, "'#{ENV[key]}'", public: true)
+		puts "Setting env var '#{key}' to '#{ENV['key']}' on project '#{repo.slug}'"
+		repo.env_vars.upsert(key, "'#{ENV['key']}'", public: true)
 	end
 end
