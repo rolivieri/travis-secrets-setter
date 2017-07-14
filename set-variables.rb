63958ef22adb9824ae8ff80d000ead7f5370a68d
rolivieri <roliv@us.ibm.com>
2017-07-13 23:00:10 -0500

    test

--- set-variables.rb
+++ set-variables.rb
@@ -7,10 +7,10 @@ puts "#{organization}"
 
 Travis.access_token = Travis.github_auth(ENV['TRAVIS_TOKEN'])
 repos = Travis::Repository.find_all(owner_name: organization)
+  .select{|repo| repo.slug == 'rolivieri/db-test'}
 	#.reject{|repo| repo.slug == 'rolivieri/travis-secrets-setter'}
 	#.reject{|repo| repo.slug == 'rolivieri/get-started-swift'}
 	#.select{|repo| Travis.user.admin_access.include?(repo)}
-	.select{|repo| repo.slug == 'rolivieri/db-test'}
 keys = ['swift-4-dev-snapshot', 'key2', 'key3']
 repos.each do |repo|
 	keys.each do |key|
